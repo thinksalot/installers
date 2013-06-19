56762f698c4ced00c9a3e23b9654430df9f20ace
Aalok T <thapa.aalok@gmail.com>
2013-06-19 19:15:47 +0545

    change ownership of www folder

--- lamp_install.sh
+++ lamp_install.sh
@@ -1,2 +1,8 @@
+# install lampp
 sudo tasksel install lamp-server
+
+# install pma
 sudo apt-get install phpmyadmin
+
+# change ownership of www folder to current user,www-date group
+sudo chown -R $(whoami):www-data /var/www
