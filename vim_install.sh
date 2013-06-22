554928a2c0db17a1acb6fbd2f2ca6f5d94cbf9ff
Aalok T <thapa.aalok@gmail.com>
2013-06-22 07:15:37 +0545

    use ssh for clone, add vimwiki and pdv

--- vim_install.sh
+++ vim_install.sh
@@ -7,7 +7,6 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 
 # install pathogen for plugin management
 # https://github.com/tpope/vim-pathogen#installation
-
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
 curl -Sso ~/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
@@ -16,14 +15,20 @@ curl -Sso ~/.vim/autoload/pathogen.vim \
 cd ~/.vim/bundle
 
 # install nerdtree
-git clone https://github.com/scrooloose/nerdtree.git
+git clone git@github.com:scrooloose/nerdtree.git
 
 # tagbar
 sudo apt-get install exuberant-ctags
-git clone git://github.com/majutsushi/tagbar
+git clone git@github.com:majutsushi/tagbar.git
 
 # powerline
 pip install --user git+git://github.com/Lokaltog/powerline
 
 # TComment
-git clone https://github.com/vim-scripts/tComment.git
+git clone git@github.com:tomtom/tcomment_vim.git
+
+# phpDocumentor
+git clone git@github.com:vim-scripts/PDV--phpDocumentor-for-Vim.git
+
+# vimwiki
+git clone git@github.com:vim-scripts/vimwiki.git
