73e771fd75bc2d776f671694a9c4b53f731689db
Aalok T <thapa.aalok@gmail.com>
2013-06-28 11:33:45 +0545

    add spacehi

--- vim_install.sh
+++ vim_install.sh
@@ -38,3 +38,6 @@ git clone git://github.com/msanders/snipmate.vim.git
 
 # css color highlight
 git clone git@github.com:skammer/vim-css-color.git
+
+# space highlight
+git clone git@github.com:jpalardy/spacehi.vim.git
