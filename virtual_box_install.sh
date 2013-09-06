7bfe0cc5dddef26cbadfaad97130e271ca10a675
thinksalot <thapa.aalok@gmail.com>
2013-09-06 22:10:21 +0545

    fix virtualbox install command

--- virtual_box_install.sh
+++ virtual_box_install.sh
@@ -1,3 +1,3 @@
 cd ~/Downloads
-wget -Ovirtualbox.deb http://download.virtualbox.org/virtualbox/4.2.12/virtualbox-4.2_4.2.12-84980~Ubuntu~raring_i386.deb
+wget -O virtualbox.deb http://download.virtualbox.org/virtualbox/4.2.12/virtualbox-4.2_4.2.12-84980~Ubuntu~raring_i386.deb
 sudo dpkg -i virtualbox.deb
