9ef94e3ca11432a6d59544736a2ba5d4e26040fd
thinksalot <thapa.aalok@gmail.com>
2015-07-31 08:49:35 +0545

    using apt to install anki

--- anki_install.sh
+++ anki_install.sh
@@ -1,7 +1,1 @@
-cd ~/Downloads
-wget https://anki.googlecode.com/files/anki-2.0.11.deb
-
-sudo dpkg -i anki-2.0.11.deb
-
-# fix missing dependencies
-sudo apt-get -f install
+sudo apt-get install anki
