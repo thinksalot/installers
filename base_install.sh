d3a04d299164664d7d27b67d0d5c2abc5afb6b3b
thinksalot <thapa.aalok@gmail.com>
2015-07-31 09:26:06 +0545

    updating skype version

--- base_install.sh
+++ base_install.sh
@@ -27,8 +27,8 @@ sudo apt-get install libgconf2-4 libxss1
 sudo dpkg -i google-chrome-stable_current_i386.deb
 
 # install skype
-wget http://download.skype.com/linux/skype-ubuntu-precise_4.2.0.11-1_i386.deb
-sudo dpkg -i skype-ubuntu-precise_4.2.0.11-1_i386.deb
+wget http://download.skype.com/linux/skype-ubuntu-lucid_4.3.0.37-1_i386.deb
+sudo dpkg -i skype-ubuntu-lucid_4.3.0.37-1_i386.deb
 
 # deluge & vlc
 sudo apt-get install deluge vlc
