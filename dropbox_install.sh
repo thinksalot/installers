cf00ed5f366108589915c70017598d274a0e5579
Aalok T <thapa.aalok@gmail.com>
2016-07-19 22:15:36 +0800

    updating dropbox installer

--- dropbox_install.sh
+++ dropbox_install.sh
@@ -1,7 +1,7 @@
 cd ~/Downloads
 
 # get and install
-wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.02.12_i386.deb -O dropbox.deb
+wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb -O dropbox.deb
 sudo dpkg -i dropbox.deb
 
 # for verifying binary signatures
